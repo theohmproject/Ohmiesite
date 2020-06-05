051b64fe4613673c8e14cba420ecebf7d2dcf7e6
Jon <squid@sqdmc.net>
2020-06-05 09:38:16 -0400

    prevent dos spam.. limit to 60 seconds per message for now.

--- scripts/daemon.py
+++ scripts/daemon.py
@@ -3,8 +3,9 @@
 import cherrypy
 import json
 import os
-from datetime import datetime
+import time
 import smtplib
+from datetime import datetime
 from email.mime.text import MIMEText
 from email.header import Header
 from email.utils import formataddr
@@ -12,6 +13,31 @@ from email.utils import formataddr
 
 # Main ohm web object
 class OhmRoot(object):
+
+    hosts = {}
+
+    def addHost(self, host):
+        ts = time.time()
+        self.hosts[host] = ts
+
+    def getHost(self, host):
+        if host in hosts:
+            return self.hosts[host];
+        return 0
+
+    def allowHost(self, host):
+        ts = time.time()
+        th = self.getHost(host)
+        if (th <= 0) :
+            return True
+        return ts - th < 60
+
+    def getHostTime(self, host):
+        ts = time.time()
+        th = self.getHost(host)
+        if (th <= 0) :
+            return 0
+        return ts - th
 
     @cherrypy.expose
     def index(self):
@@ -27,6 +53,8 @@ class OhmRoot(object):
         cl = cherrypy.request.headers['Content-Length']
         rawbody = cherrypy.request.body.read(int(cl))
         body = json.loads(rawbody)
+        remoteHost = cherrypy.request.headers['X_FORWARDED_FOR'];
+        print( "Request from " + remoteHost )
         # process form data..
         captcha = body['g-recaptcha-response']
         name = body['Name']
@@ -36,16 +64,24 @@ class OhmRoot(object):
         print ( "RECEIVED FROM: " + email + "  MESSAGE: " + message )
         dateTimeObj = datetime.now() # current time
         # build email body..
-        msg = str(dateTimeObj) + " > " + "NAME: " + name + "  EMAIL: " + email + " <br/>\n" + message + "";
-        # send mail
-        self.sendMail(name, email, msg)
-        # log to file..
-        file1 = open("contact.log", "a")  # append mode
-        file1.write(str(dateTimeObj) + " > " + "NAME: " + name + ", EMAIL: " + email + ", MESSAGE: " + message + "\n")
-        file1.close()
-        # send response
-        message = { "response" : True, "status" : True, "message" : "message sent!" }
-        return json.dumps(message)
+        msg = "SERVER TIME: " + str(dateTimeObj) + "\n" + "NAME: " + name + "\nEMAIL: " + email + "\n\n" + message + "";
+        # Check if can send..
+        if self.allowHost(remoteHost):
+            # send mail
+            self.sendMail(name, email, msg)
+            # track host..
+            addHost(remoteHost)
+            # log to file..
+            file1 = open("contact.log", "a")  # append mode
+            file1.write(str(dateTimeObj) + " > " + "NAME: " + name + ", EMAIL: " + email + ", MESSAGE: " + message + "\n")
+            file1.close()
+            # send response
+            message = { "response" : True, "status" : True, "message" : "message sent!" }
+            return json.dumps(message)
+        else:
+            # send response
+            message = { "response" : False, "status" : True, "message" : "you are doing this too often!", "retry" : getHostTime(remoteHost) }
+            return json.dumps(message)
 
     #@cherrypy.expose
     #def shutdown(self):

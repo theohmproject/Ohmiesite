66c36d0b1805069a857dfce3a9fcabc3da1523ce
Jon <squid@sqdmc.net>
2020-06-05 17:03:21 -0400

    daemon cleanups; error trapping

--- scripts/daemon.py
+++ scripts/daemon.py
@@ -23,7 +23,7 @@ class OhmRoot(object):
 
     @cherrypy.expose
     def index(self):
-        message = { "response" : True, "status" : True, "message" : "not a web server." }
+        message = { "response" : True, "status" : True, "message" : "Not a web server. This method is restricted." }
         return json.dumps(message)
 
     @cherrypy.expose
@@ -31,32 +31,49 @@ class OhmRoot(object):
         if (cherrypy.request.method == "GET") :
             message = { "response" : True, "status" : False, "message" : "post required.." }
             return json.dumps(message)
+        # setup default vars
+        body = json.loads("{}")
+        remoteHost = "127.0.0.1"
+        userAgent = "1"
+        agentHash = "0"
+        hostHash = "0"
         # load form data
-        cl = cherrypy.request.headers['Content-Length']
-        rawbody = cherrypy.request.body.read(int(cl))
-        body = json.loads(rawbody)
-        remoteHost = cherrypy.request.headers['X-FORWARDED-FOR']
-        userAgent = cherrypy.request.headers['USER-AGENT']
-        agentHash = self.getHostHash(remoteHost, userAgent)
-        hostHash = self.getHostHash(remoteHost, 0)
+        try:
+            cl = cherrypy.request.headers['Content-Length']
+            rawbody = cherrypy.request.body.read(int(cl))
+            body = json.loads(rawbody)
+            remoteHost = cherrypy.request.headers['X-FORWARDED-FOR']
+            userAgent = cherrypy.request.headers['USER-AGENT']
+            # get agent hashes..
+            agentHash = self.getHostHash(remoteHost, userAgent)
+            hostHash = self.getHostHash(remoteHost, 0)
+        except ex:
+            print("ERROR: " + ex)
+            message = { "response" : False, "status" : False, "message" : "input error.." }
+            return json.dumps(message)
         # Check if spam..
         if self.allowHost(hostHash) == False:
             message = { "response" : False, "status" : True, "message" : "you are doing this too often!!" }
             return json.dumps(message)
+        elif remoteHost == "127.0.0.1":
+            message = { "response" : False, "status" : False, "message" : "host invalid.." }
+            return json.dumps(message)
         else:
             self.addHost(hostHash)
+        # clean up old agent trackings..
+        self.cleanHostAgents()
         # process form data..
         captcha = body['g-recaptcha-response']
         name = body['Name']
         email = body['Email']
         message = body['Message']
         # log to console..
-        print ( "RECEIVED FROM: " + email + "  MESSAGE: " + message )
-        dateTimeObj = datetime.now() # current time
-        # build email body..
-        msg = "\nUSER HOST: " + hostHash + "\nUSER AGENT: " + agentHash + "\nSERVER TIME: " + str(dateTimeObj) + "\n\nFROM NAME: " + name + "\nFROM EMAIL: " + email + "\n\nMESSAGE:\n" + message + "";
+        print ( "RECEIVED CONTACT FROM: " + email + "  MESSAGE: " + message )
         # Check if can send..
         if self.allowAgent(agentHash):
+            dateTimeObj = datetime.now()  # current time
+            # build email body..
+            msg = "\nUSER HOST: " + hostHash + "\nUSER AGENT: " + agentHash + "\nSERVER TIME: " + str(dateTimeObj) + "\n\nFROM NAME: " + name + "\nFROM EMAIL: " + email + "\n\nMESSAGE:\n" + message + "";
             # send mail
             self.sendMail(name, email, msg)
             # track host..
@@ -89,6 +106,25 @@ class OhmRoot(object):
         s.sendmail(xfrom, xto, msg.as_string())
         s.quit()
         print( "Mail Sent!" )
+
+    # Host Agent cleaner
+    def cleanHostAgents(self):
+        hosts = []
+        agents = []
+        # Clean Hosts
+        for key in self.hosts:
+            if self.getHostTime(key) <= -30:
+                hosts.push(key)
+        for key in hosts:
+            self.hosts.pop(key)
+            print( "> Removed Expired Host " + key )
+        # Clean Agents
+        for key in self.hostsAgents:
+            if self.getAgentTime(key) <= -30:
+                agents.push(key)
+        for key in agents:
+            self.hostsAgents.pop(key)
+            print( "> Removed Expired Agent " + key )
 
     # Hosts
     def addHost(self, host):

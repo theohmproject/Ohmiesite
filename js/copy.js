30cd39967731f51efdcb16875c34f53d68fcc191
crytpoplissken <rasalghul@ohmcoin.org>
2020-12-28 07:48:02 -0800

    Update Karmanode Guide Intro
    This begins the updated karmanode install guild and adds javascript for copy and paste for the rest of the guide.
    
    This part of the guide is for desktop viewing only. A mobile viewport guide will be made the "old school" way but should not add to much load time to the page as it will be mostly text.
    
    Note: downloadable pdf guide should be added for download and a video guild needs to be created to help the visual learners :)

---
+++ js/copy.js
@@ -0,0 +1,11 @@
+<script>
+function CopyToClipboard(id)
+{
+var r = document.createRange();
+r.selectNode(document.getElementById(id));
+window.getSelection().removeAllRanges();
+window.getSelection().addRange(r);
+document.execCommand('copy');
+window.getSelection().removeAllRanges();
+}
+</script>
